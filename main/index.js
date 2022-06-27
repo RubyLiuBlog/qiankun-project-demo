817111593c9f778d25c4365c4f43dd6c73f75435
rubyliu <liulubingjava@163.com>
2022-06-28 01:34:05 +0800

    添加主应用路由

--- main/index.js
+++ main/index.js
@@ -1,17 +1,18 @@
-import { registerMicroApps, start } from 'qiankun';
+// import { registerMicroApps, start } from 'qiankun';
+import { registerMicroApps, start } from 'rubyliu-micro-front';
 import './index.less';
 
 /**
- * Step2 注册子应用
+ * Step1 注册子应用
  */
 
 registerMicroApps(
   [
     {
       name: 'react',
-      entry: '//localhost:7100',
-      container: '#subapp-container',
-      activeRule: '/react',
+      entry: '//localhost:7100', // 子应用的 HTML 入口
+      container: '#subapp-container',  // 渲染到哪里
+      activeRule: '/react', // 路由匹配规则
     },
     {
       name: 'vue2',
@@ -29,7 +30,7 @@ registerMicroApps(
 );
 
 /**
- * Step4 启动应用
+ * Step2 启动应用
  */
 start();
 
